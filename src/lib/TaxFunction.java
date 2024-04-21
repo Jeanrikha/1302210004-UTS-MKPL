d8225948c74d23d7bbadf625b3dece37520db1ad
Jeanrikha <jeanrikha@student.telkomuniversity.ac.id>
2024-04-21 18:10:29 +0700

    Menghapus comments pada TaxFunction
    Menghapus comment dengan cara menulis code dengan jelas  sehingga orang lain dapat memahaminya hanya dengan melihat algoritma yang disajikan.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,18 +1,6 @@
 package lib;
 
 public class TaxFunction {
-
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
 	
 	 private static final int TAX_PERCENTAGE = 5;
 	 private static final int NON_TAXABLE_INCOME_SINGLE = 54000000;
