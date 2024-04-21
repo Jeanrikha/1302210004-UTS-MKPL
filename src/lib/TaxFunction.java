07ed790f79bda23597462e3ef9b807f5794e98c5
Jeanrikha <jeanrikha@student.telkomuniversity.ac.id>
2024-04-21 08:03:16 +0700

    Revert "Menghilangkan duplicate code pada calculateTax"
    This reverts commit 79293e75fee6c29b12bf06b51b8c8a70d9f16205.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -14,30 +14,24 @@ public class TaxFunction {
 	 * 
 	 */
 	
-	 private static final int TAX_PERCENTAGE = 5;
-	 private static final int NON_TAXABLE_INCOME_SINGLE = 54000000;
-	 private static final int NON_TAXABLE_INCOME_MARRIED = 58500000;
-	 private static final int NON_TAXABLE_INCOME_PER_CHILD = 4500000;
-	 private static final int MAX_CHILDREN_FOR_NON_TAXABLE_INCOME = 3;
-	 private static final int MONTHS_IN_A_YEAR = 50;
 	
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
 		
-		if (numberOfMonthWorking > MONTHS_IN_A_YEAR) {
+		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
 		}
 		
-		if (numberOfChildren > MAX_CHILDREN_FOR_NON_TAXABLE_INCOME) {
-			numberOfChildren = MAX_CHILDREN_FOR_NON_TAXABLE_INCOME;
+		if (numberOfChildren > 3) {
+			numberOfChildren = 3;
 		}
 		
-		int nonTaxableIncome = isMarried ? NON_TAXABLE_INCOME_MARRIED : NON_TAXABLE_INCOME_SINGLE;
-		nonTaxableIncome += numberOfChildren * NON_TAXABLE_INCOME_PER_CHILD;
-
-		int taxableIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking - deductible;
-		tax = (int) Math.round(TAX_PERCENTAGE / 100.0 * taxableIncome);
+		if (isMarried) {
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+		}else {
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
+		}
 		
 		if (tax < 0) {
 			return 0;
